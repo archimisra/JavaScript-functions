9bd6bd5070e35e1da5921c9dd910a07f52a615b1
Archi Misra <misraarchi117@gmail.com>
2026-03-12 00:26:15 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,17 @@
+function myFunction(){
+    console.log("Welcome to Archi's Page🫧");
+    console.log("This Site is Made of Js, HTML, CSS");
+    console.log("Hope you enjoy💋");
+}
+myFunction();
+myFunction();
+myFunction();//will print the commands 3 times.
+
+function Sum(x,y){
+    s = x+y;
+    console.log("before return");
+    return s;
+    console.log("after return");
+}
+let Val = Sum(3,4)
+console.log(Val);
